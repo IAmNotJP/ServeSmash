eca7d351691289ce33c5556800cac131cb580045
jlpzcp <jamari1998@live.com>
2025-03-06 14:40:59 -0600

    Character Basics Done

--- SmashServe/Characters/Volleyballer/volleyballer.gd
+++ SmashServe/Characters/Volleyballer/volleyballer.gd
@@ -1,17 +1,15 @@
 extends CharacterBody2D
 
-const UP = Vector2(0,-1)
-const SPEED = 80
-const JUMP_VELOCITY = -400.0
-var motion = Vector2()
+@export_subgroup("Nodes")
+@export var gravity_component: GravityComponent
+@export var input_component: InputComponent
+@export var movement_component: MovementComponent
+@export var animation_component: AnimationComponent
 
+var team: String = 'red'
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if Input.is_action_pressed("right"):
-		motion.x = SPEED
-	if Input.is_action_pressed("left"):
-		motion.x = -SPEED
-	else:
-		motion.x =0
-		
+	gravity_component.handle_gravity(self,delta)
+	movement_component.handle_horizontal_movement(self, input_component.input_horizontal)
+	animation_component.handle_move_animation(input_component.input_horizontal)
+	move_and_slide()
---
+++ SmashServe/Scenes/Components/animation_component.gd
@@ -0,0 +1,12 @@
+class_name AnimationComponent
+extends Node
+
+@export_subgroup("Nodes")
+@export var sprite: AnimatedSprite2D
+
+func handle_move_animation(move_direction: float) -> void:
+	if move_direction != 0:
+		sprite.play('run')
+	else:
+		sprite.play('idle')
+	
