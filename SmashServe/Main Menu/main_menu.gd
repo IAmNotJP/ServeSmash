2367e675367662cf98ff1486dbee398402005958
Dfratila <60486392+Dfratila@users.noreply.github.com>
2025-04-07 15:38:15 -0500

    exit fix

--- SmashServe/Main Menu/main_menu.gd
+++ SmashServe/Main Menu/main_menu.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var asp: AudioStreamPlayer = $AudioStreamPlayer
 @onready var logo: Sprite2D = $CenterContainer/Sprite2D
 @onready var flash_animation: AnimationPlayer = $CenterContainer/Sprite2D/AnimationPlayer
+@onready var next_scene: PackedScene = preload("res://Stages/Stage1.tscn")
 
 func _ready():
 	logo.scale = Vector2(0.245, 0.245)  # Shrinks sprite to 50% of original size
@@ -11,7 +12,7 @@ func _ready():
 
 func _on_texture_button_1_pressed() -> void:
 	asp.stop()
-	get_tree().change_scene_to_file("res://Stages/Stage1.tscn")
+	get_tree().change_scene_to_packed(next_scene)
 	
 
 
