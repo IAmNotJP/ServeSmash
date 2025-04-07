7d8f329fcbfabd2a1b18254cb89be80ebefc5749
jlpzcp <jamari1998@live.com>
2025-04-06 22:08:31 -0500

    lol

--- SmashServe/Scenes/Components/input_component.gd
+++ SmashServe/Scenes/Components/input_component.gd
@@ -5,16 +5,16 @@ var input_horizontal: float = 0.0
 var input_jump: bool = false
 
 func _process(_delta: float) -> void:
-	input_horizontal = Input.get_axis('p1left','p1right')
+	input_horizontal = Input.get_axis(get_parent().inputs['left'],get_parent().inputs['right'])
 	
 func get_jump_input() -> bool:
-	return Input.is_action_just_pressed("p1jump")	
+	return Input.is_action_just_pressed(get_parent().inputs['jump'])	
 
 func get_volley_input()-> bool:
-	return Input.is_action_just_pressed("p1volley")
+	return Input.is_action_just_pressed(get_parent().inputs['volley'])
 
 func get_smash_input()-> bool:
-	return Input.is_action_just_pressed("p1smash")
+	return Input.is_action_just_pressed(get_parent().inputs['smash'])
 	
 func get_lob_input()-> bool:
-	return Input.is_action_just_pressed("p1lob")
+	return Input.is_action_just_pressed(get_parent().inputs['lob'])
