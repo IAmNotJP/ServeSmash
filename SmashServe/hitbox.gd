0ec7e5387ccbeceeec98aedb7996d8a3915128f9
jlpzcp <jamari1998@live.com>
2025-04-08 11:44:24 -0500

    DemoStage+Collision gameplay done
    Everything for a base stage is done except scoring but that is kinda easy to implement

--- SmashServe/hitbox.gd
+++ SmashServe/hitbox.gd
@@ -21,4 +21,4 @@ func _on_body_entered(body: Node2D) -> void:
 	print(body)
 	if body is CharacterBody2D and get_parent().linear_velocity.length() > 100 and not body == last_player:
 		body.call_deferred("die")
-	pass # Replace with function body.
+	pass # .Replace with function body.
