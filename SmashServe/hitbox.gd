6cf7d1f524325f35ca505bbc24782f95968a0c16
meow56 <spinattacktotheface@gmail.com>
2025-04-22 14:09:55 -0500

    Ball effects
    Added a trail to the ball that changes color the faster it goes. Also separated the glow effect so that it could be potentially customized in-game. Finally, got started on the fail states of letting the ball bounce twice.

--- SmashServe/hitbox.gd
+++ SmashServe/hitbox.gd
@@ -1,17 +1,46 @@
 extends Area2D
 
 const MVEL_MULT = 1.05
+const GRAD_VEL_START = 1000
+const GRAD_VEL_END = 5000
+const GRAD_VEL_END_START = 2000
 
 var last_player
+var num_bounces = 0
 var max_velocity = 500
 
+var prev_positions = []
 
+func _process(_delta: float):
+	prev_positions.push_front(global_position)
+	if prev_positions.size() > 20:
+		prev_positions.pop_back()
+		
+	#var test = Sprite2D.new()
+	#test.texture = ColorRect.new()
+	#test.texture
+	#test.position = position
+	for i in range(prev_positions.size()):
+		get_node("../Line2D").set_point_position(i, prev_positions[i] - global_position)
+	
+	if get_parent().linear_velocity.length() > GRAD_VEL_START:
+		var start_scaling_factor = clamp(get_parent().linear_velocity.length() - GRAD_VEL_START, 0, GRAD_VEL_END) / GRAD_VEL_END
+		var end_scaling_factor = clamp(get_parent().linear_velocity.length() - GRAD_VEL_END_START, 0, GRAD_VEL_END) / GRAD_VEL_END
+		
+		var grad = Gradient.new()
+		grad.set_color(0, Color.from_rgba8(128 + (128 * start_scaling_factor), 255 - (255 * start_scaling_factor), 0))
+		grad.set_color(1, Color.from_rgba8(128 + (128 * end_scaling_factor), 255 - (255 * end_scaling_factor), 0))
+		get_node("../Line2D").set_gradient(grad)
+	else:
+		get_node("../Line2D").set_gradient(null)
+	pass
 
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.get_parent() is CharacterBody2D:
 		max_velocity *= MVEL_MULT
 		last_player = area.get_parent()
+		num_bounces = 0
 		area.get_parent().volley_component.call_deferred("ball_hit", get_parent(), max_velocity)
 		area.get_parent().smash_component.call_deferred("ball_hit", get_parent(), max_velocity)
 		area.get_parent().lob_component.call_deferred("ball_hit", get_parent(), max_velocity)
@@ -19,6 +48,8 @@ func _on_area_entered(area: Area2D) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	print(body)
-	if body is CharacterBody2D and get_parent().linear_velocity.length() > 100 and not body == last_player:
-		body.call_deferred("die")
+	if body is not CharacterBody2D:
+		num_bounces += 1
+	if num_bounces >= 2:
+		pass # Award a point to the last player who hit the ball
 	pass # Replace with function body.
