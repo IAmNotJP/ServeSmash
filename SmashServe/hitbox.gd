a04f6d12177cbc70f1b18bd0282dd354e3e0a626
meow56 <spinattacktotheface@gmail.com>
2025-05-05 01:00:03 -0500

    Points and Win Condition
    Added the ability to win the game. Also added ball resetting after a point is scored. It still needs to run initial setup when the stage first starts to decide who gets to serve first. It also needs to actually do the graphics stuff, and I need to adjust the ball so it actually goes over the net when you hit it, or we need to adjust the point system itself.

--- SmashServe/hitbox.gd
+++ SmashServe/hitbox.gd
@@ -1,13 +1,16 @@
 extends Area2D
 
+signal score(last_hitter: CharacterBody2D, ball: RigidBody2D)
+
 const MVEL_MULT = 1.05
 const GRAD_VEL_START = 750
 const GRAD_VEL_END = 1500
 const GRAD_VEL_END_START = 1000
+const INIT_MAX_VEL = 500
 
-var last_player
+var last_player: CharacterBody2D = null
 var num_bounces = 0
-var max_velocity = 500
+var max_velocity = INIT_MAX_VEL
 
 var prev_positions = []
 
@@ -35,8 +38,9 @@ func _process(_delta: float):
 func _on_area_entered(area: Area2D) -> void:
 	if area.get_parent() is CharacterBody2D:
 		max_velocity *= MVEL_MULT
+		if last_player != area.get_parent():
+			num_bounces = 0
 		last_player = area.get_parent()
-		num_bounces = 0
 		area.get_parent().volley_component.call_deferred("ball_hit", get_parent(), max_velocity)
 		area.get_parent().smash_component.call_deferred("ball_hit", get_parent(), max_velocity)
 		area.get_parent().lob_component.call_deferred("ball_hit", get_parent(), max_velocity)
@@ -46,6 +50,17 @@ func _on_body_entered(body: Node2D) -> void:
 	#print(body)
 	if body is not CharacterBody2D:
 		num_bounces += 1
-	if num_bounces >= 2:
-		pass # Award a point to the last player who hit the ball
+	if num_bounces >= 2 and last_player != null:
+		score.emit(last_player, get_parent())
 	pass # Replace with function body.
+
+
+func reset_internals() -> void:
+	print("RESETTING")
+	num_bounces = 0
+	max_velocity = INIT_MAX_VEL
+	prev_positions = []
+	var trail: Line2D = get_node("../Line2D")
+	for i in range(trail.get_point_count()):
+		trail.set_point_position(i, Vector2(0, 0))
+	last_player = null
