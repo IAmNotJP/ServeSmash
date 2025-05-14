454de49ab14a06bb2c609dddb5fb1209f08a0214
Dfratila <60486392+Dfratila@users.noreply.github.com>
2025-05-14 17:35:28 -0500

    Bo3 implemented

--- SmashServe/Stages/DemoStageChars/game_controller.gd
+++ SmashServe/Stages/DemoStageChars/game_controller.gd
@@ -6,6 +6,8 @@ extends Node2D
 
 @onready var p1 = $"../Player1Init".get_child(0)
 @onready var p2 = $"../Player2Init".get_child(0)
+@onready var p_1_games: Label = $P1Games
+@onready var p_2_games: Label = $P2Games
 
 @onready var left_is_p1 = p1.position.x < DisplayServer.window_get_size(0).x / 2
 
@@ -24,6 +26,11 @@ var scored = false
 var p1_serves = RandomNumberGenerator.new().randf() < 0.5
 
 func ball_reset(ball: RigidBody2D):
+	Engine.time_scale = 0.1 # slowdown effects are cool right?
+	await get_tree().create_timer(5.0, true, false, true).timeout
+	Engine.time_scale = 1
+	scored = false
+	
 	resetting = true
 	if p1_serves:
 		ball.global_position = p1_ball_spawn
@@ -58,27 +65,54 @@ func _on_ball_score(last_hitter: CharacterBody2D, ball: RigidBody2D) -> void:
 	
 	
 	if p1_score > 3:
-		win(true)
+		if p1_games == 1:
+			win(true)
+		else:
+			win_game(true)
+			ball_reset(ball)
 	elif p2_score > 3:
-		win(false)
+		if p1_games == 1:
+			win(false)
+		else:
+			win_game(false)
+			ball_reset(ball)
 	else:
 		print("Current score: ", p1_score, "-", p2_score) # display the score
-		Engine.time_scale = 0.1 # slowdown effects are cool right?
-		await get_tree().create_timer(5.0, true, false, true).timeout
-		Engine.time_scale = 1
-		scored = false
 		ball_reset(ball)
 		
+
 func _input(ev):
 	if Input.is_action_just_pressed("Reset"):
 		Engine.time_scale = 1
 		get_tree().reload_current_scene()
 
+func win_game(p1_won: bool) -> void:
+	if p1_won: # Do the displaying of player 1 or 2 winning
+		print("Player 1 won the game!")
+		p1_games += 1
+		p_1_games.text = "◍○"
+	else: 
+		print("Player 2 won the game!")
+		p2_games += 1
+		p_2_games.text = "○◍"
+	
+	Engine.time_scale = 0.1
+	await get_tree().create_timer(5.0, true, false, true).timeout
+	Engine.time_scale = 1
+
+	p1_score = 0
+	p2_score = 0
+
+
 func win(p1_won: bool) -> void:
 	if p1_won: # Do the displaying of player 1 or 2 winning
-		print("Player 1 won!")
+		print("Player 1 won the set!")
+		p1_games += 1
+		p_1_games.text = "◍◍"
 	else: 
-		print("Player 2 won!")
+		print("Player 2 won the set!")
+		p2_games += 1
+		p_2_games.text = "◍◍"
 	
 	Engine.time_scale = 0.1
 	await get_tree().create_timer(5.0, true, false, true).timeout
--- SmashServe/Stages/DemoStageChars/game_status_check.gd
+++ SmashServe/Stages/DemoStageChars/game_status_check.gd
@@ -1,31 +1,33 @@
 extends Label
 
 @onready var p1score = get_parent().p1_score
-
+@onready var p1games = get_parent().p1_games
 @onready var p2score = get_parent().p2_score
-
+@onready var p2games = get_parent().p2_games
 
 func _process(delta: float) -> void:
 	
 	if get_parent().scored:
+		p1score = get_parent().p1_score
+		p1games = get_parent().p1_games
+		p2score = get_parent().p2_score
+		p2games = get_parent().p2_games
 		visible = true
+		
 		if(get_parent().p1_score > 3):
-			text = "P1 Wins!"
-			p1score = get_parent().p1_score
-			await get_tree().create_timer(5).timeout
-		#elif(get_parent().p1_score > p1score):
-			#text = "P1 Scored!"
-			#p1score = get_parent().p1_score
-			#await get_tree().create_timer(3).timeout
-		
-		if(get_parent().p2_score > 3):
-			text = "P2 Wins!"
-			p2score = get_parent().p2_score
-			await get_tree().create_timer(5).timeout
-		#elif(get_parent().p2_score > p2score):
-			#text = "P2 Scored!"
-			#p2score = get_parent().p2_score
-			#await get_tree().create_timer(3).timeout
-		
+			if p1games > 1:
+				text = "P1 Wins!"
+				await get_tree().create_timer(5).timeout
+			else:
+				text = "P1 Game!"
+				await get_tree().create_timer(2).timeout
+		elif(get_parent().p2_score > 3):
+			if p2games > 1:
+				text = "P2 Wins!"
+				await get_tree().create_timer(5).timeout
+			else:
+				text = "P2 Game!"
+				await get_tree().create_timer(2).timeout
+
 		visible = false
 		
