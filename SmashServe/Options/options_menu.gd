945904e58abd25be50ff158b6e7bdafef3e5b054
Dfratila <60486392+Dfratila@users.noreply.github.com>
2025-05-14 11:23:49 -0500

    options fix
    fixed options plus the wolf sprite issue

--- SmashServe/Options/options_menu.gd
+++ SmashServe/Options/options_menu.gd
@@ -1,10 +1,16 @@
 extends PanelContainer
 
 @onready var exit: Button = $Exit
+@onready var master_volume_slider: HSlider = $TabContainer/Sound/MarginContainer/VBoxContainer/MasterLabel/MasterVolumeSlider
+@onready var music_volume_slider: HSlider = $TabContainer/Sound/MarginContainer/VBoxContainer/MusicLabel/MusicVolumeSlider
+@onready var sfx_volume_slider: HSlider = $TabContainer/Sound/MarginContainer/VBoxContainer/SFXLabel/SFXVolumeSlider
 
 signal exit_options_menu
 
 func _ready() -> void:
+	master_volume_slider.set("bus_index", 0)
+	music_volume_slider.set("bus_index", 2)
+	sfx_volume_slider.set("bus_index", 1)
 	exit.pressed.connect(on_exit_pressed)
 	set_process(false)
 	
