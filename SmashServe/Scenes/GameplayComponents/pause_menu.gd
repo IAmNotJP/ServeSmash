f9157a67c72df0467cc4fc266728436816449d19
jlpzcp <jamari1998@live.com>
2025-05-15 15:40:11 -0500

    quick charlie fix

--- SmashServe/Scenes/GameplayComponents/pause_menu.gd
+++ SmashServe/Scenes/GameplayComponents/pause_menu.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
-
 func _input(_event):
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
 	if Input.is_action_just_pressed("pause"):
 		if get_tree().paused == false:
 			get_tree().paused = true		
+			
 		else:
 			get_tree().paused = false
